d0afd62b0180213bdf7ac17a916db449915381a8
tderamus <deramust@gmail.com>
2025-04-17 19:23:03 -0500

    updated endpoint files to complete job and user endpoints

--- EndPoints/JobEndpoints.cs
+++ EndPoints/JobEndpoints.cs
@@ -2,3 +2,36 @@ using KindQuest.Interfaces;
 using KindQuest.Models;
 
 namespace KindQuest.Endpoints;
+
+public static class JobEndpoints
+{
+    public static void MapJobEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/jobs").WithTags("Jobs");
+        group.MapGet("/", async (IJobRepository jobService) =>
+        {
+            return await jobService.GetAllAsync();
+        })
+        .WithName("GetAllJobs")
+        .Produces<List<Job>>(StatusCodes.Status200OK);
+        group.MapGet("/{id}", async (int id, IJobRepository jobService) =>
+        {
+            return await jobService.GetByIdAsync(id);
+        })
+        .WithName("GetJobById")
+        .Produces<Job>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+        group.MapPost("/", async (Job job, IJobRepository jobService) =>
+        {
+            return await jobService.CreateAsync(job);
+        });
+        group.MapPut("/{id}", async (int id, Job job, IJobRepository jobService) =>
+        {
+            return await jobService.UpdateAsync(id, job);
+        });
+        group.MapDelete("/{id}", async (int id, IJobRepository jobService) =>
+        {
+            return await jobService.DeleteAsync(id);
+        });
+    }
+}
