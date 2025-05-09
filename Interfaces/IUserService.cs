ecf61a4c319e75e8acd5916f33184ab8a77ce436
Josh Gochey <josh@gochey.net>
2025-05-08 20:39:30 -0500

    Unit tests for User Entity

--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -5,7 +5,7 @@ namespace KindQuest.Interfaces
 {
     public interface IUserService
     {
-        Task<User> GetUserByIdAsync(int id);
+        Task<User?> GetUserByIdAsync(int id);
         Task<IEnumerable<User>> GetAllUsersAsync();
         Task<User> CreateUserAsync(User user);
         Task<User> UpdateUserAsync(int Id, User user);
